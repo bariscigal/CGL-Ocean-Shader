dddbe8786c5f73f219d23dec33a5579df63ef5b9
Baris Cigal <bariscigal@gmail.com>
2021-04-24 15:10:47 +0300

    Add files via upload

---
+++ Assets/Scripts/AnimatedTextureManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedTextureManager : MonoBehaviour
+{
+
+    public Texture[] texturesArr;
+    Material _myMat;
+    int currentTexId = 0;
+
+    void Start()
+    {
+        _myMat = GetComponent<MeshRenderer>().sharedMaterial;
+        StartCoroutine(AnimateTexture());
+    }
+
+    IEnumerator AnimateTexture()
+    {
+        while (true) {
+            currentTexId++;
+            if (currentTexId >= texturesArr.Length) currentTexId = 0;
+            _myMat.SetTexture("_BaseMap", texturesArr[currentTexId]);
+            yield return new WaitForSeconds(0.08f);
+        }
+    }
+   
+}
